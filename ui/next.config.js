c97dfaa74b35881e9b12e6c8204d467c0754ef01
Mark Moffat <hi@markmoffat.com>
2022-06-25 12:45:57 +0930

    Moved to preact to reduce bundle size

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     distDir: process.env.BUILD_DIR,
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+                react: 'preact/compat',
+                'react-dom/test-utils': 'preact/test-utils',
+                'react-dom': 'preact/compat',
+            })
+        }
+        return config
+    },
     async rewrites() {
         return [
             {
