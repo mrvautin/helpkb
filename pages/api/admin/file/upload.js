2677ec856d6cf93bb4b1f1a6d49f87a1ab3d3113
Mark Moffat <hi@markmoffat.com>
2023-05-23 17:16:12 +0930

    Mime type and file size checking on API route

--- pages/api/admin/file/upload.js
+++ pages/api/admin/file/upload.js
@@ -30,11 +30,34 @@ export default async function handler(req, res) {
 
         // Setup the S3 upload
         const file = formfields.image;
+
+        // Check file size (bytes) - Defaults to 2mb
+        if (file.size > 2000000) {
+            res.status(400).send({
+                message: 'File size exceeds the 2mb limit',
+            });
+        }
+
         const fileExt = (/[^./\\]*$/.exec(file.originalFilename) || [''])[0];
         const s3Client = new S3Client({});
         const s3Bucket = process.env.S3_BUCKET_NAME;
         const fileUUID = uuidv4();
         const fileName = `${fileUUID}.${fileExt}`;
+
+        // Check the mimetype of the file upload
+        const allowedTypes = [
+            'image/jpeg',
+            'image/jpg',
+            'image/png',
+            'image/gif',
+            'image/bmp',
+        ];
+        if (!allowedTypes.includes(file.mimetype)) {
+            res.status(400).send({
+                message:
+                    'File type not supported. Supported types: jpeg, jpg, png, gif, bmp',
+            });
+        }
 
         // Upload the file
         const uploadCommand = new PutObjectCommand({
@@ -53,7 +76,7 @@ export default async function handler(req, res) {
         return res.json(response);
     } catch (ex) {
         console.log('Error uploading file', ex);
-        return res.json({
+        return res.status(400).json({
             error: 'Unable to upload file. Please check your config and try again.',
         });
     }
