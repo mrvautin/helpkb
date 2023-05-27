238d9105725b45e948ac1e12d6c874fb0d631533
Mark Moffat <hi@markmoffat.com>
2023-05-27 17:52:58 +0930

    Fixing sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,35 @@
+/** @type {import('next-sitemap').IConfig} */
+const prisma = require('./lib/prisma');
+
 module.exports = {
     siteUrl: process.env.HOST_NAME,
+    exclude: [
+        '/login',
+        '/admin/dashboard',
+        '/admin/categories',
+        '/admin/menu',
+        '/admin/new',
+        '/admin/settings',
+        '/admin/users',
+    ],
+    additionalPaths: async () => {
+        const pages = [];
+
+        // Get our articles from the DB
+        const articles = await prisma.articles.findMany({
+            where: {
+                published: true,
+            },
+        });
+        articles.forEach(article => {
+            pages.push({ loc: `/article/${article.url}` });
+        });
+
+        // Get our categories from the DB
+        const categories = await prisma.categories.findMany();
+        categories.forEach(category => {
+            pages.push({ loc: `/category/${category.url}` });
+        });
+        return pages;
+    },
 };
